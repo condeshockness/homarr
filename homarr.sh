a29cf0c790054379133acc11d5b2cf825b2dc719
condeshockness <ramonsilas.dev@gmail.com>
2025-05-30 10:57:14 -0300

    ok

---
+++ homarr.sh
@@ -0,0 +1,203 @@
+#!/usr/bin/env bash
+# Author: Ramon
+
+# This function sets various color variables using ANSI escape codes for formatting text in the terminal.
+color() {
+  # Colors
+  YW=$(echo "\033[33m")
+  YWB=$(echo "\033[93m")
+  BL=$(echo "\033[36m")
+  RD=$(echo "\033[01;31m")
+  BGN=$(echo "\033[4;92m")
+  GN=$(echo "\033[1;92m")
+  DGN=$(echo "\033[32m")
+
+  # Formatting
+  CL=$(echo "\033[m")
+  BOLD=$(echo "\033[1m")
+  HOLD=" "
+  TAB="  "
+  TAB3="      "
+
+  # Icons
+  CM="${TAB}✔️${TAB}"
+  CROSS="${TAB}✖️${TAB}"
+  INFO="${TAB}💡${TAB}${CL}"
+  OS="${TAB}🖥️${TAB}${CL}"
+  OSVERSION="${TAB}🌟${TAB}${CL}"
+  CONTAINERTYPE="${TAB}📦${TAB}${CL}"
+  DISKSIZE="${TAB}💾${TAB}${CL}"
+  CPUCORE="${TAB}🧠${TAB}${CL}"
+  RAMSIZE="${TAB}🛠️${TAB}${CL}"
+  SEARCH="${TAB}🔍${TAB}${CL}"
+  VERBOSE_CROPPED="🔍${TAB}"
+  VERIFYPW="${TAB}🔐${TAB}${CL}"
+  CONTAINERID="${TAB}🆔${TAB}${CL}"
+  HOSTNAME="${TAB}🏠${TAB}${CL}"
+  BRIDGE="${TAB}🌉${TAB}${CL}"
+  NETWORK="${TAB}📡${TAB}${CL}"
+  GATEWAY="${TAB}🌐${TAB}${CL}"
+  DISABLEIPV6="${TAB}🚫${TAB}${CL}"
+  DEFAULT="${TAB}⚙️${TAB}${CL}"
+  MACADDRESS="${TAB}🔗${TAB}${CL}"
+  VLANTAG="${TAB}🏷️${TAB}${CL}"
+  ROOTSSH="${TAB}🔑${TAB}${CL}"
+  CREATING="${TAB}🚀${TAB}${CL}"
+  ADVANCED="${TAB}🧩${TAB}${CL}"
+  FUSE="${TAB}🗂️${TAB}${CL}"
+}
+
+APP="homarr"
+
+header_info "$APP"
+
+msg_error() {
+  stop_spinner
+  local msg="$1"
+  printf "\r\e[2K%s %b\n" "${CROSS}" "${RD}${msg}${CL}" >&2
+}
+
+variables
+color
+catch_errors
+
+  if [[ ! -d /opt/homarr ]]; then
+    msg_error "No ${APP} Installation Found!"
+    exit
+  fi
+  if [[ -f /opt/homarr/database/db.sqlite ]]; then
+    msg_error "Old Homarr detected due to existing database file (/opt/homarr/database/db.sqlite)."
+    msg_error "Update not supported. Refer to:"
+    msg_error " - https://github.com/community-scripts/ProxmoxVE/discussions/1551"
+    msg_error " - https://homarr.dev/docs/getting-started/after-the-installation/#importing-a-zip-from-version-before-100"
+    exit 1
+  fi
+  if [[ ! -f /opt/run_homarr.sh ]]; then
+    msg_info "Detected outdated and missing service files"
+    msg_error "Warning - The port of homarr changed from 3000 to 7575"
+    $STD apt-get install -y nginx gettext openssl gpg
+    sed -i '/^NODE_ENV=/d' /opt/homarr/.env && echo "NODE_ENV='production'" >>/opt/homarr/.env
+    sed -i '/^DB_DIALECT=/d' /opt/homarr/.env && echo "DB_DIALECT='sqlite'" >>/opt/homarr/.env
+    cat <<'EOF' >/opt/run_homarr.sh
+#!/bin/bash
+set -a
+source /opt/homarr/.env
+set +a
+export DB_DIALECT='sqlite'
+export AUTH_SECRET=$(openssl rand -base64 32)
+node /opt/homarr_db/migrations/$DB_DIALECT/migrate.cjs /opt/homarr_db/migrations/$DB_DIALECT
+for dir in $(find /opt/homarr_db/migrations/migrations -mindepth 1 -maxdepth 1 -type d); do
+  dirname=$(basename "$dir")
+  mkdir -p "/opt/homarr_db/migrations/$dirname"
+  cp -r "$dir"/* "/opt/homarr_db/migrations/$dirname/" 2>/dev/null || true
+done
+export HOSTNAME=$(ip route get 1.1.1.1 | grep -oP 'src \K[^ ]+')
+envsubst '${HOSTNAME}' < /etc/nginx/templates/nginx.conf > /etc/nginx/nginx.conf
+nginx -g 'daemon off;' &
+redis-server /opt/homarr/packages/redis/redis.conf &
+node apps/tasks/tasks.cjs &
+node apps/websocket/wssServer.cjs &
+node apps/nextjs/server.js & PID=$!
+wait $PID
+EOF
+    chmod +x /opt/run_homarr.sh
+    rm /etc/systemd/system/homarr.service
+    cat <<EOF >/etc/systemd/system/homarr.service
+[Unit]
+Description=Homarr Service
+After=network.target
+[Service]
+Type=exec
+WorkingDirectory=/opt/homarr
+EnvironmentFile=-/opt/homarr/.env
+ExecStart=/opt/run_homarr.sh
+[Install]
+WantedBy=multi-user.target
+EOF
+    msg_ok "Updated Services"
+    systemctl daemon-reload
+  fi
+  RELEASE=$(curl -fsSL https://api.github.com/repos/homarr-labs/homarr/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
+  if [[ ! -f /opt/${APP}_version.txt ]] || [[ "${RELEASE}" != "$(cat /opt/${APP}_version.txt)" ]]; then
+
+    msg_info "Stopping Services (Patience)"
+    systemctl stop homarr
+    msg_ok "Services Stopped"
+
+    msg_info "Backup Data"
+    mkdir -p /opt/homarr-data-backup
+    cp /opt/homarr/.env /opt/homarr-data-backup/.env
+    msg_ok "Backup Data"
+
+    msg_info "Updating and rebuilding ${APP} to v${RELEASE} (Patience)"
+    rm /opt/run_homarr.sh
+    cat <<'EOF' >/opt/run_homarr.sh
+#!/bin/bash
+set -a
+source /opt/homarr/.env
+set +a
+export DB_DIALECT='sqlite'
+export AUTH_SECRET=$(openssl rand -base64 32)
+node /opt/homarr_db/migrations/$DB_DIALECT/migrate.cjs /opt/homarr_db/migrations/$DB_DIALECT
+for dir in $(find /opt/homarr_db/migrations/migrations -mindepth 1 -maxdepth 1 -type d); do
+  dirname=$(basename "$dir")
+  mkdir -p "/opt/homarr_db/migrations/$dirname"
+  cp -r "$dir"/* "/opt/homarr_db/migrations/$dirname/" 2>/dev/null || true
+done
+export HOSTNAME=$(ip route get 1.1.1.1 | grep -oP 'src \K[^ ]+')
+envsubst '${HOSTNAME}' < /etc/nginx/templates/nginx.conf > /etc/nginx/nginx.conf
+nginx -g 'daemon off;' &
+redis-server /opt/homarr/packages/redis/redis.conf &
+node apps/tasks/tasks.cjs &
+node apps/websocket/wssServer.cjs &
+node apps/nextjs/server.js & PID=$!
+wait $PID
+EOF
+    chmod +x /opt/run_homarr.sh
+    $STD command -v jq || $STD apt-get update && $STD apt-get install -y jq
+    NODE_VERSION=$(curl -s https://raw.githubusercontent.com/homarr-labs/homarr/dev/package.json | jq -r '.engines.node | split(">=")[1] | split(".")[0]')
+    NODE_MODULE="pnpm@$(curl -s https://raw.githubusercontent.com/homarr-labs/homarr/dev/package.json | jq -r '.packageManager | split("@")[1]')"
+    install_node_and_modules
+    rm -rf /opt/homarr
+    fetch_and_deploy_gh_release "homarr-labs/homarr"
+    mv /opt/homarr-data-backup/.env /opt/homarr/.env
+    cd /opt/homarr
+    $STD pnpm install --recursive --frozen-lockfile --shamefully-hoist
+    $STD pnpm build
+    cp /opt/homarr/apps/nextjs/next.config.ts .
+    cp /opt/homarr/apps/nextjs/package.json .
+    cp -r /opt/homarr/packages/db/migrations /opt/homarr_db/migrations
+    cp -r /opt/homarr/apps/nextjs/.next/standalone/* /opt/homarr
+    mkdir -p /appdata/redis
+    cp /opt/homarr/packages/redis/redis.conf /opt/homarr/redis.conf
+    rm /etc/nginx/nginx.conf
+    mkdir -p /etc/nginx/templates
+    cp /opt/homarr/nginx.conf /etc/nginx/templates/nginx.conf
+
+    mkdir -p /opt/homarr/apps/cli
+    cp /opt/homarr/packages/cli/cli.cjs /opt/homarr/apps/cli/cli.cjs
+    echo $'#!/bin/bash\ncd /opt/homarr/apps/cli && node ./cli.cjs "$@"' >/usr/bin/homarr
+    chmod +x /usr/bin/homarr
+
+    mkdir /opt/homarr/build
+    cp ./node_modules/better-sqlite3/build/Release/better_sqlite3.node ./build/better_sqlite3.node
+    echo "${RELEASE}" >/opt/${APP}_version.txt
+    msg_ok "Updated ${APP}"
+
+    msg_info "Starting Services"
+    systemctl start homarr
+    msg_ok "Started Services"
+    msg_ok "Updated Successfully"
+    read -p "${TAB3}It's recommended to reboot the LXC after an update, would you like to reboot the LXC now ? (y/n): " choice
+    if [[ "$choice" =~ ^[Yy]$ ]]; then
+      reboot
+    fi
+  else
+    msg_ok "No update required. ${APP} is already at v${RELEASE}"
+  fi
+  exit
+
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Access it using the following URL:${CL}"
+echo -e "${TAB}${GATEWAY}${BGN}http://${IP}:7575${CL}"
